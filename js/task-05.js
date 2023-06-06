24c5a25f2ebc6e377cc585a3f26efa770ebae3c1
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2023-06-06 07:57:58 +0300

    commit-6

---
+++ js/task-05.js
@@ -0,0 +1,17 @@
+/* const textInput = document.querySelector('#name-input')
+const output = document.querySelector('#name-output')
+
+textInput.addEventListener('input', event => {
+	output.textContent = event.target.value
+
+	if (event.target.value === '') {
+		output.textContent = 'Anonymous'
+	}
+}) */
+
+const textInput = document.querySelector('#name-input')
+const output = document.querySelector('#name-output')
+
+textInput.addEventListener('input', event => {
+    
+})
